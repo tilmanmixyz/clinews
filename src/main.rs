76ab92425ec90699d1ed708747895233cbfc4321
Tilman Andre Mix <tilmanmixyz@proton.me>
2023-05-13 19:28:58 +0200

    updating tokio becuase of vulnerability

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,10 @@ fn render_articles(articles: &Vec<Article>) {
 async fn main() -> Result<(), Box<dyn Error>> {
     let api_key: &str = "f5fd5e5064bf44ae862e45f6a08c945a";
 
-    let mut newsapi = NewsApi::new(&api_key);
+    let mut newsapi = NewsApi::new(api_key);
     newsapi.endpoint(Endpoint::TopHeadlines).country(Country::De);
 
     let articles = newsapi.fetch_async().await?;
-
 
     render_articles(articles.articles());
     println!("News are provided by https://newsapi.org");
