51197081d814623614a6510beb6f2fb86e69003b
Tilman Andre Mix <tilmanmixyz@proton.me>
2023-06-12 17:02:30 +0200

    updating gitignore

--- flake.nix
+++ flake.nix
@@ -1,38 +1,46 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = {self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-      {
-        packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "clinews";
-          version = "0.2.0";
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
-          buildInputs = with pkgs; [
-            openssl
-          ];
-        };
-        devShells.${system}.default = pkgs.mkShell {
-          packages = with pkgs; [
-            bacon
-          ];
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
-          buildInputs = with pkgs; [
-            cargo
-            openssl
-          ];
-        };
-      };
+  outputs = {self, nixpkgs, fenix, naersk, flake-utils }:
+  flake-utils.lib.eachDefaultSystem(system:
+  let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+    rustToolchain = fenix.packages.${system}.fromToolchainFile {
+      dir = ./.;
+      sha256 = pkgs.lib.fakeSha256;
+    };
+    naersk-lib = naersk.lib.${system}.override {
+      cargo = rustToolchain;
+      rustc = rustToolchain;
+    };
+    nativeBuildInputs = with pkgs; [
+      pkg-config
+      rustToolchain
+    ];
+    buildInputs = with pkgs; [
+      openssl
+    ];
+    bin = naersk-lib.buildPackage {
+      src = ./.;
+      doCheck = true;
+      pname = "clinews";
+      version = "0.1.0";
+      inherit nativeBuildInputs buildInputs;
+    };
+  in
+  {
+    packages = {
+      inherit bin;
+      default = bin;
+    };
+  });
 }
